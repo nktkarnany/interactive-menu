253f9c8ffd96ad3eb9a4c681b7e30776ca4f5927
Ankit Karnany <ankit@greylabs.org>
2018-06-14 09:24:42 +0530

    basic working navigation header

---
+++ index.js
@@ -0,0 +1,9 @@
+(function () {
+  function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
+})();
