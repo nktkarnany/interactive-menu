ad2fb3d73c2a23105c50f62afdbafc218fa1fa7b
Ankit Karnany <ankit@greylabs.org>
2018-06-14 22:44:03 +0530

    raw navbar created

--- index.js
+++ index.js
@@ -4,6 +4,5 @@
 
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
   }
-  document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
+//  document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
 })();
