780e31fbd457a67f0e1df54cf6352da97686756d
Ankit Karnany <ankit@greylabs.org>
2018-06-17 01:43:34 +0530

    basic toggle implemented

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 (function () {
   function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
+    const navs = document.querySelectorAll('.nav-toggle');
 
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+    navs.forEach(nav => nav.classList.toggle('show'));
   }
-//  document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
+  document.querySelector('.toggle').addEventListener('click', classToggle);
 })();
